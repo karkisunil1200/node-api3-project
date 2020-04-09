a4838a080e1565e8d3b87002498f87c6131abfa0
Sunil Karki <karkisunil1200@gmail.com>
2020-04-09 16:36:35 -0500

    Added express json

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const postRouter = require('./posts/postRouter');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 server.use('/api/posts', postRouter);
+server.use('/api/user', postRouter);
 
 //custom middleware
 
